5ae22d8c88ae0d0f80fa63cefe9ed4595a99187e
Artur <radioshake@ya.ru>
2021-03-12 18:55:08 +0300

    add lesson 1

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
+    { title: 'Shirt', price: 150, img: "img/noimg.jpg" },
+    { title: 'Socks', price: 50, img: "img/noimg.jpg" },
+    { title: 'Jacket', price: 350, img: "img/noimg.jpg" },
+    { title: 'Shoes', price: 250, img: "img/noimg.jpg" },
 ];
 
 
 const $goodsList = document.querySelector('.goods-list');
-  
-const renderGoodsItem = ({ title, price }) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+
+const renderGoodsItem = ({ title, price, img }) => {
+    return `<div class="goods-item"><img src="${img}"><h3>${title}</h3><p>${price} $</p><button class="add_cart" type="button">В корзину</button></div>`;
 };
-  
+
 const renderGoodsList = (list = goods) => {
     let goodsList = list.map(
-            item => renderGoodsItem(item)
-        );
+        item => renderGoodsItem(item)
+    ).join('\n'); 
 
     $goodsList.insertAdjacentHTML('beforeend', goodsList);
 }
-  
+
+
+class GetCart {
+    constructor() {}
+}
+
+
+class GetItem {
+    constructor() {}
+    getSumm(){
+        let fullSumm = 0;
+        goods.forEach(items => {
+            fullSumm += items.price;
+        });
+        return fullSumm;
+    }
+}
+
+console.log(GetItem.prototype.getSumm());
 renderGoodsList();
